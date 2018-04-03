f86c9f6e8be4d9d063c171b0a13eab39cb6f3e2e
Marina Valiquette <marina.valiquette@gmail.com>
2018-04-03 13:01:54 -0400

    Fix bugs in min and max

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function max(numbers) {
 	const arrLength = numbers.length;
 
   if(arrLength > 0) {
-    let maxVal = 0;
+    let maxVal = numbers[0];
     let count = 0;
     while(count < arrLength) {
     	if (maxVal < numbers[count]) {
@@ -18,83 +18,22 @@ function max(numbers) {
 }
 
 function min(numbers) {
+	const arrLength = numbers.length;
   if(numbers.length > 0) {
-    let minVal = 0;
+    let minVal = numbers[0];
 	let count = 0;
     while(count < arrLength) {
-    	if (manVal < numbers[count]) {
-        	manVal = numbers[count];
+    	if (minVal > numbers[count]) {
+        	minVal = numbers[count];
     	}
     	count++;
     }
     return minVal;
   } else {
     return undefined;
-  }
-    
+  }   
 }
 
-/* From here down, you are not expected to 
-   understand.... for now :)  
-   
-   
-   Nothing to see here!
-   
-*/
+console.log(max([1,2,3]));
+console.log(min([1,2,3]));
 
-// tests
-
-function testFunctionWorks(fn, input, expected) {
-  if (fn(input) === expected) {
-    console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
-    return true;
-  } else {
-    console.log(
-      'FAILURE: `' +
-        fn.name +
-        '([' +
-        input +
-        '])` should be ' +
-        expected +
-        ' but was ' +
-        fn(input)
-    );
-    return false;
-  }
-}
-
-function testEmpty(fn) {
-  if (fn([]) === null || fn([]) == undefined) {
-    console.log(`SUCCESS: ${fn.name} works on empty arrays`);
-    return true;
-  } else {
-    console.log(
-      `FAILURE: ${fn.name} should return undefined or null for empty arrays`
-    );
-    return false;
-  }
-}
-
-(function runTests() {
-  // we'll use the variables in our test cases
-  const numList1 = [-5, 28, 98, -20013, 0.7878, 22, 115];
-  const realMin1 = numList1[3];
-  const realMax1 = numList1[6];
-  const numList2 = [0, 1, 2, 3, 4];
-  const realMin2 = numList2[0];
-  const realMax2 = numList2[4];
-
-  const testResults = [
-    testFunctionWorks(max, numList1, realMax1),
-    testFunctionWorks(max, numList2, realMax2),
-    testFunctionWorks(min, numList1, realMin1),
-    testFunctionWorks(min, numList2, realMin2),
-    testEmpty(max),
-    testEmpty(min),
-  ];
-
-  const numPassing = testResults.filter(function(result) {
-    return result;
-  }).length;
-  console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
-})();
